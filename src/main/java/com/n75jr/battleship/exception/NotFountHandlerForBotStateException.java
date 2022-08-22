b8d5275cec27a9ff0ff184f13520b7cdf0254c2b
Roman Nikitin <n75rom@gmail.com>
2022-08-22 14:34:01 +0900

    feat: add NotFountHandlerForBotStateException

---
+++ src/main/java/com/n75jr/battleship/exception/NotFountHandlerForBotStateException.java
@@ -0,0 +1,11 @@
+package com.n75jr.battleship.exception;
+
+public class NotFountHandlerForBotStateException extends RuntimeException {
+
+    public NotFountHandlerForBotStateException() {
+    }
+
+    public NotFountHandlerForBotStateException(String message) {
+        super(message);
+    }
+}
