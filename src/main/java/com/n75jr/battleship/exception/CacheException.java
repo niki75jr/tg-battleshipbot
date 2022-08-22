6183bea1336a7ab888e152bf773896881131d1e4
Roman Nikitin <n75rom@gmail.com>
2022-08-22 14:33:44 +0900

    feat: add CacheException

---
+++ src/main/java/com/n75jr/battleship/exception/CacheException.java
@@ -0,0 +1,11 @@
+package com.n75jr.battleship.exception;
+
+public class CacheException extends RuntimeException {
+
+    public CacheException() {
+    }
+
+    public CacheException(String message) {
+        super(message);
+    }
+}
